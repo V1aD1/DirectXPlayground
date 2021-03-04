12f1e85f1dcca417aed630dfc382ba2aa794e2de
Vlad <vladfriends@yahoo.ca>
2021-03-04 09:32:26 +0100

    Created a hot coloured triangle

--- DirectXPlayground/VertexShader.hlsl
+++ DirectXPlayground/VertexShader.hlsl
@@ -7,9 +7,10 @@ VOut main(float4 pos : POSITION, float4 color: COLOR)
 {
 	VOut output;
 	output.position = pos;
+	output.color = color;
 
-	// inverts the colors of the triangle
-	output.color = 1.0f - color;
+	// creating hot colors
+	output.color.r = 1.0f;
 
 	// order of return values MUST MATCH order of input variables to pixel shader!
 	return output;
