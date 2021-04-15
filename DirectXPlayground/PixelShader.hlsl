48d88d1313c0e4d599e0fdcc481fdcf3d2b8cce7
Vlad <vladfriends@yahoo.ca>
2021-04-15 08:05:21 +0200

    comments for later

--- DirectXPlayground/PixelShader.hlsl
+++ DirectXPlayground/PixelShader.hlsl
@@ -1,3 +1,5 @@
+// due to optimizations, Texture1 isn't visible in the HLSL debugger...
+// have to pass debug flag D3DCOMPILE_SKIP_OPTIMIZATION when debugging shaders?
 Texture2D<float4> Texture1: register(t0);
 Texture2D<float4> Texture2: register(t1);
 
