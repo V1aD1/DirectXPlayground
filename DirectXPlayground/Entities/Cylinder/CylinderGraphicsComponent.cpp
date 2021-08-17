e62336afafdc626bbff222b36e3eb92adf991ea9
Vlad <vladfriends@yahoo.ca>
2021-08-17 07:59:35 +0200

    Continued progress on cylinder

--- DirectXPlayground/Entities/Cylinder/CylinderGraphicsComponent.cpp
+++ DirectXPlayground/Entities/Cylinder/CylinderGraphicsComponent.cpp
@@ -1,44 +1,85 @@
 #include "pch.h"
 #include "CylinderGraphicsComponent.h"
 
+#include <math.h>
 
 CylinderGraphicsComponent::CylinderGraphicsComponent(float radius, float height, int numStacks, int numSlices)
 {
 	// cylinder grows in height from its starting position
 
-	// first, create a diagonal box
-	float halfRadius = radius / 2.0f; 
-
 	// use numSlices to determine the bottom points around the radius to build the cylinder
 	float sliceAngle = 360.0f / numSlices;
 
-	for (int i = 0; i < numSlices; ++i) {
-		float currAngle = i + sliceAngle;
-		// hypotenuse = halfRadius
+	for (int i = 0; i < numSlices - 1; ++i) {
+		float startAngle = i * sliceAngle;
+		float endAngle = (i + 1) * sliceAngle;
+
+		// hypotenuse = radius
 		// using sine, compute length of opposite = equals our x value = sin(currAngle) * hypotenuse
 		// using cosine, compute length of opposite = equals our z value = cos(currAngle) * hypotenuse
+
+		auto x1 = sin(startAngle) * radius;
+		auto z1 = cos(startAngle) * radius;
+		auto x2 = sin(endAngle) * radius;
+		auto z2 = cos(endAngle) * radius;
+
+
+		auto bottomLeft = Vector3(x1, 0.0f, z1);
+		auto bottomRight = Vector3(x2, 0.0f, z2);
+
+		// in order to compute normal, simply take vector from center of cylinder (0,0,0) to middle of current face
+		// therefore normal is just bottom right corner - bottom left corner!!
+		auto normal = bottomRight - bottomLeft;
+
+		m_vertices.push_back({ x1, 0.0f, z1,     normal.x, normal.y, normal.z,   0.0f, 0.0f });
+		m_vertices.push_back({ x1, height, z1,   normal.x, normal.y, normal.z,   0.0f, 1.0f });
+		m_vertices.push_back({ x2, height, z2,   normal.x, normal.y, normal.z,   1.0f, 1.0f });
+		m_vertices.push_back({ x2, 0.0f, z2,     normal.x, normal.y, normal.z,   1.0f, 0.0f });
 	}
 
-	// in order to compute normal, simply take vector from center of cylinder (0,0,0) to middle of current face
-	// therefore normal is just bottom right corner - bottom left corner!!
-	auto bottomLeft = Vector3{ 0.0f, 0.0f, -halfRadius };
-	auto bottomRight = Vector3{ halfRadius, 0.0f, 0.0f };
+	for (int i = 0; i < m_vertices.size(); i+=4) {
+		m_indices.push_back(i);
+		m_indices.push_back(i+1);
+		m_indices.push_back(i+2);
+		m_indices.push_back(i+2);
+		m_indices.push_back(i+3);
+		m_indices.push_back(i);
+	}
 
-	auto normal = bottomRight - bottomLeft;
-
-	m_vertices.push_back({ 0.0f, 0.0f, -halfRadius,        normal.x, normal.y, normal.z,   0.0f, 0.0f });
-	m_vertices.push_back({ 0.0f, height, -halfRadius,      normal.x, normal.y, normal.z,   0.0f, 1.0f });
-	m_vertices.push_back({ halfRadius, height, 0.0f,       normal.x, normal.y, normal.z,   1.0f, 1.0f });
-	m_vertices.push_back({ halfRadius, 0.0f, 0.0f,         normal.x, normal.y, normal.z,   1.0f, 0.0f });
-
-	m_indices.push_back(0);
-	m_indices.push_back(1);
-	m_indices.push_back(2);
-	m_indices.push_back(2);
-	m_indices.push_back(3);
-	m_indices.push_back(0);
+	int coversStartIndex = m_vertices.size();
 
 	// todo add top and bottom covers
+	for (int i = 0; i < numSlices - 1; ++i) {
+		float startAngle = i * sliceAngle;
+		float endAngle = (i + 1) * sliceAngle;
+
+		auto x1 = sin(startAngle) * radius;
+		auto z1 = cos(startAngle) * radius;
+		auto x2 = sin(endAngle) * radius;
+		auto z2 = cos(endAngle) * radius;
+
+		// top cover
+		// todo u and v coordinates
+		m_vertices.push_back({ 0.0f, height, 0.0f,  0.0f, 1.0f, 0.0f,    0.0f, 0.0f });
+		m_vertices.push_back({x1, height, z1,       0.0f, 1.0f, 0.0f,    0.0f, 0.0f});
+		m_vertices.push_back({x2, height, z2,       0.0f, 1.0f, 0.0f,    0.0f, 0.0f});
+
+		// bottom cover
+		// todo u and v coordinates
+		m_vertices.push_back({ 0.0f, 0.0f, 0.0f,   0.0f, -1.0f, 0.0f,    0.0f, 0.0f });
+		m_vertices.push_back({ x1, 0.0f, z1,       0.0f, -1.0f, 0.0f,    0.0f, 0.0f });
+		m_vertices.push_back({ x2, 0.0f, z2,       0.0f, -1.0f, 0.0f,    0.0f, 0.0f });
+	}
+
+	for (int i = coversStartIndex; i < m_vertices.size(); i += 6) {
+		m_indices.push_back(i);
+		m_indices.push_back(i+1);
+		m_indices.push_back(i+2);
+
+		m_indices.push_back(i + 3);
+		m_indices.push_back(i + 4);
+		m_indices.push_back(i + 5);
+	}
 
 	SetupBuffers();
 }
